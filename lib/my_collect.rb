5bc3cf6e22d922b8f6d362de17a160bfcdd308d4
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-27 17:44:00 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+def my_collect(collection)
+  counter = 0
+  new_collection = []
+  if collection == [] 
+    new_collection = []
+  else 
+    collection_count = collection.size  
+    while counter < collection_count
+      yield collection[counter]
+      new_collection << collection[counter]
+      counter += 1
+    end
+  end
+  new_collection
+end
 
+my_each(1) {|i| "#{i}"}
 
